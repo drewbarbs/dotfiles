3b201e71013f8c4ceba524e6743294ba6b404974
Andrew Barbarello <andrew.barbarello@outlook.com>
2018-10-13 16:34:46 -0400

    Cleanup arch update mon: do everything with GLib functions
    Remove hacky timer thread thing/python signal handler

--- xmonad/.config/xmobar/arch-updates-mon.py
+++ xmonad/.config/xmobar/arch-updates-mon.py
@@ -5,14 +5,12 @@
 # https://github.com/RaphaelRochet/arch-update
 
 import argparse
-import atexit
 import functools
 import signal
 import subprocess
 import sys
 from contextlib import suppress
 from datetime import timedelta
-from threading import Lock, Thread, Timer
 
 import gi
 gi.require_version('GLib', '2.0')
@@ -21,60 +19,40 @@ gi.require_version('Gio', '2.0')
 from gi.repository import GLib
 from gi.repository import Gio
 
-CHECK_LOCK = Lock()
 DEFAULT_CHECK_PERIOD = timedelta(hours=1)
 
 DEFAULT_CHECK_CMD = '/usr/bin/checkupdates'
 DEFAULT_WATCH_DIR = '/var/lib/pacman/local'
 
 
-def tryprint(s):
+def tryprint(*args, **kwargs):
     with suppress(Exception):
-        print(s)
+        print(*args, **kwargs)
 
 
-def check_for_updates(check_cmd, *args):
-    if args and len(args) >= 4 and isinstance(args[3], Gio.FileMonitorEvent):
-        *rest, evt = args
-        # File monitor doesnt seem to emit changes_done_hint if the
-        # only changes were deletes?
-        if evt not in (Gio.FileMonitorEvent.CHANGES_DONE_HINT,
-                       Gio.FileMonitorEvent.DELETED):
-            return
-
-    def wrap_refresh_action(s):
-        return '<action=`pkill -10 -f arch-updates-mon.py` button=2>' + s + '</action>'
+def check_for_updates(check_cmd, *ignored):
+    tryprint('<action=`pkill -10 -f arch-updates-mon.py` button=2>', end='')
 
     try:
-        with CHECK_LOCK:
-            output_bytes = subprocess.check_output(check_cmd, shell=True)
+        output_bytes = subprocess.check_output(check_cmd, shell=True)
     except:
-        tryprint(wrap_refresh_action('<fc=#FFB6B0><icon=arch-error-symbolic.xbm/>ERROR</fc>'))
+        tryprint(
+            '<fc=#FFB6B0><icon=arch-error-symbolic.xbm/>ERROR</fc>', end='')
     else:
         output = output_bytes.decode('utf8')
         if output:
             n_avail = len(output.split('\n')) - 1
-            tryprint(wrap_refresh_action(
-                "<action=`bash -c 'zenity --text-info --filename=<(checkupdates)'` button=1>"
-                "<fc=#CEFFAC><icon=arch-updates-symbolic.xbm/></fc> {}</action>"
-            ).format(n_avail))
+            tryprint(
+                ("<action=`bash -c 'zenity --text-info --filename=<(checkupdates)'` button=1>"
+                 "<fc=#CEFFAC><icon=arch-updates-symbolic.xbm/></fc> {}</action>"
+                 ).format(n_avail),
+                end='')
         else:
-            tryprint(wrap_refresh_action('<icon=arch-uptodate-symbolic.xbm/>'))
+            tryprint('<icon=arch-uptodate-symbolic.xbm/>', end='')
 
+    tryprint('</action>')
     with suppress(BrokenPipeError):
         sys.stdout.flush()
-
-
-def start_next_timer(check_period, *args):
-    timer_thread = Timer(
-        check_period.total_seconds(), timer_func, args=(check_period, ) + args)
-    timer_thread.daemon = True
-    timer_thread.start()
-
-
-def timer_func(check_period, check_cmd):
-    check_for_updates(check_cmd)
-    start_next_timer(check_period, check_cmd)
 
 
 if __name__ == '__main__':
@@ -99,26 +77,42 @@ if __name__ == '__main__':
         default=DEFAULT_CHECK_PERIOD.total_seconds())
     args = p.parse_args()
 
-    start_next_timer(timedelta(seconds=args.interval), args.cmd)
+    do_check = functools.partial(check_for_updates, args.cmd)
 
-    check_for_updates(args.cmd)
-
-    gfile = Gio.file_new_for_path(args.dir)
-    monitor = gfile.monitor_directory(Gio.FileMonitorFlags.NONE)
-    monitor.connect('changed', functools.partial(check_for_updates, args.cmd))
-    atexit.register(lambda: monitor.cancel())
+    def on_filemon_evt(monitor, file, other_file, event_type, *ignored):
+        # File monitor doesnt seem to emit changes_done_hint if the
+        # only changes were deletes?
+        if event_type in (Gio.FileMonitorEvent.CHANGES_DONE_HINT,
+                          Gio.FileMonitorEvent.DELETED):
+            do_check()
 
     ml = GLib.MainLoop()
+    gfile = Gio.file_new_for_path(args.dir)
+    monitor = gfile.monitor_directory(Gio.FileMonitorFlags.NONE)
+    monitor.connect('changed', on_filemon_evt)
 
-    GLib.unix_fd_add_full(GLib.PRIORITY_DEFAULT, sys.stdout.fileno(), 0,
-                          lambda *args, **kwargs: ml.quit())
+    GLib.io_add_watch(sys.stdout, GLib.PRIORITY_DEFAULT,
+                      GLib.IO_HUP | GLib.IO_ERR, lambda *args: ml.quit())
 
-    signal.signal(signal.SIGUSR1, functools.partial(check_for_updates,
-                                                    args.cmd))
+    def handle_glib_event_repeat(*args):
+        # GLib expects this to return True if we want this function to
+        # be called repeatedly (after every timeout/signal delivery)
+        do_check()
+        return True
 
-    ml.run()
+    GLib.timeout_add(1000 * args.interval, handle_glib_event_repeat)
+    GLib.unix_signal_add(GLib.PRIORITY_DEFAULT, signal.SIGUSR1,
+                         handle_glib_event_repeat)
 
-    # Finally, close stdout ourselves so we can swallow exception from stdout flush
+    try:
+        do_check()
+        ml.run()
+    finally:
+        monitor.cancel()
+
+    # Finally, close stdout ourselves so we can swallow exception from
+    # stdout flush (since we only reach this point if the other end of
+    # stdout pipe has been closed)
     # https://stackoverflow.com/a/18954489/756104
     with suppress(Exception):
         sys.stdout.close()
