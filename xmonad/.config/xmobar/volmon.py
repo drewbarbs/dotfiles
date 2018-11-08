ad430d70b63e9d8ab510d2cc2a6890a8dfac465e
Andrew Barbarello <andrew.barbarello@outlook.com>
2018-11-07 20:37:35 -0500

    rewrite xmobar pulseaudio volume monitor with asyncio
    Gets rid of need for a separate thread to monitor for when stdout
    closes, and relaunches pactl if a previous instance dies (e.g. on
    pulseaudio restart)

--- xmonad/.config/xmobar/volmon.py
+++ xmonad/.config/xmobar/volmon.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
+import asyncio
 import subprocess
-import threading
+import sys
 
-from stdout_watch import watch_for_stdout_close
-
-def proc_lines(p):
-    while True:
-        line = p.stdout.readline().decode('utf8')
-        if line == '':
-            break
-        yield line
 
 def update_volume():
-    vol, state = subprocess.check_output("amixer get Master | awk -F'[][%]' '/Front Left:/ { print $2, $5 }'",
-                                         shell=True).decode('utf8').split()
+    vol, state = subprocess.check_output(
+        "amixer get Master | awk -F'[][%]' '/Front Left:/ { print $2, $5 }'",
+        shell=True).decode('utf8').split()
     if state == 'on':
         print('<fc=#CEFFAC><icon=spkr_01.xbm/></fc>', end='')
     else:
@@ -21,13 +15,55 @@ def update_volume():
 
     print(' {}%'.format(vol), flush=True)
 
-update_volume()
-p = subprocess.Popen(['pactl', 'subscribe'], stdout=subprocess.PIPE)
 
-stdout_close_watcher = threading.Thread(target=watch_for_stdout_close, args=(lambda: p.kill(),))
-stdout_close_watcher.deamon = True
-stdout_close_watcher.start()
+class StdoutProtocol(asyncio.Protocol):
+    def __init__(self, exit_future):
+        self.exit_future = exit_future
 
-for l in proc_lines(p):
-    if 'sink' in l:
-        update_volume()
+    def connection_lost(self, exc):
+        # Detects stdout close *when stdout is a PIPE*
+        self.exit_future.set_result(True)
+
+
+async def monitor(exit_future):
+    while not exit_future.done():
+        # pactl will die if pulseaudio is restarted, so we'll
+        # continually relaunch (as I sometimes need to restart pulse
+        # if sound gets weird)
+        mon = await asyncio.create_subprocess_exec(
+            'pactl',
+            'subscribe',
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.DEVNULL,
+            stdin=asyncio.subprocess.DEVNULL)
+
+        while True:
+            ltask = asyncio.create_task(mon.stdout.readline())
+            done, pending = await asyncio.wait(
+                [exit_future, ltask], return_when=asyncio.FIRST_COMPLETED)
+            if exit_future in done:
+                mon.kill()
+                break
+            line = ltask.result().decode('utf8')
+            if not line:
+                break
+            if 'sink' in line:
+                update_volume()
+
+        await mon.wait()
+
+
+async def main():
+    loop = asyncio.get_event_loop()
+    exit_future = loop.create_future()
+    coro = loop.connect_write_pipe(lambda: StdoutProtocol(exit_future),
+                                   sys.stdout)
+    stdout_watch_task = loop.create_task(coro)
+    try:
+        await asyncio.gather(stdout_watch_task, monitor(exit_future))
+    except:
+        stdout_watch_task.cancel()
+
+
+update_volume()  # Initial print of volume status
+asyncio.run(main())
