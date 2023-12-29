5410e02feefae6980de69612df99e0c372f5d435
Andrew Barbarello <andrew.barbarello@outlook.com>
2023-12-29 16:44:12 -0500

    Fixup swayidle config

--- hyprland/.local/bin/run_swayidle.sh
+++ hyprland/.local/bin/run_swayidle.sh
@@ -4,5 +4,4 @@ swayidle -w \
          timeout  900 'swaylock -f -c 000000' \
          timeout 1200 'hyprctl dispatch dpms off' \
          timeout 1500 'systemctl suspend' \
-               resume 'hyprctl dispatch dpms on' \
-         before-sleep 'swaylock -f -c 000000'
+               resume 'hyprctl dispatch dpms on'
